a923093bd24f9081a5c163975c4f04b8c5bb0002
CraigChilds94 <childscraig17@gmail.com>
2014-06-11 13:49:59 +0100

    3 Lines brah

--- browser.py
+++ browser.py
@@ -1,4 +1,3 @@
 #!/usr/bin/env python
 import sys, os
-for a in ["default", "safari", "firefox", "opera"]:
-	os.system("open" + (" -a " + a if a != "default" else "") + " " + (sys.argv[1] if sys.argv[1] else "index.html"))
+for a in ["default", "safari", "firefox", "opera"]: os.system("open" + (" -a " + a if a != "default" else "") + " " + (sys.argv[1] if sys.argv[1] else "index.html"))
