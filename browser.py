54058f48b403ac1839623e969f971d871686c80d
CraigChilds94 <childscraig17@gmail.com>
2014-06-11 11:13:44 +0100

    Boom 7 lines

--- browser.py
+++ browser.py
@@ -1,15 +1,7 @@
 #!/usr/bin/env python
 import sys, os
-
 args = sys.argv
-file_url = ""
-
-if(args[1]):
-	file_url = args[1]
-else:
-	file_url = "index.html"
-
+file_url = args[1] if args[1] else "index.html"
 os.system("open " + file_url)
-os.system("open -a safari " + file_url)
-os.system("open -a firefox " + file_url)
-os.system("open -a opera " + file_url)
+for a in ["safari", "firefox", "opera"]:
+	os.system("open -a " + a + " " + file_url)
