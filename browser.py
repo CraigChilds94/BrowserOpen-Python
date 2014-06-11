2a92983a29a5ac076444637d386744296b0ae44a
CraigChilds94 <childscraig17@gmail.com>
2014-06-11 11:16:08 +0100

    6 LIIIINNNNESSS

--- browser.py
+++ browser.py
@@ -2,6 +2,5 @@
 import sys, os
 args = sys.argv
 file_url = args[1] if args[1] else "index.html"
-os.system("open " + file_url)
-for a in ["safari", "firefox", "opera"]:
-	os.system("open -a " + a + " " + file_url)
+for a in ["default", "safari", "firefox", "opera"]:
+	os.system("open" + (" -a " + a if a != "default" else "") + " " + file_url)
